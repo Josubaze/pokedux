0cbdb67fd6f556bc6b8806a73da7677545ba21e5
Josue Baez <josuejbaeze4gmail.com>
2024-07-18 17:10:17 -0400

    Redux DevTools

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import { Provider } from 'react-redux';
 import { legacy_createStore as createStore } from 'redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const state = createStore(pokemonsReducer)
+const state = createStore(
+  pokemonsReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 
 root.render(
   <React.StrictMode>
